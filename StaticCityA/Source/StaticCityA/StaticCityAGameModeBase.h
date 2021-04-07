0f76a0dae498e599d45aec400ce38ddc19e75cbd
John <81230008+Suocixers@users.noreply.github.com>
2021-04-07 01:51:31 -0400

    Initial Commit
    Testing commits to GitHub made a shit ass player character. Going to fix PC(Player Character) after messing with blender more.
    
    Co-Authored-By: Exceptis #0001 <59620169+NtLoadDriverEx@users.noreply.github.com>

---
+++ StaticCityA/Source/StaticCityA/StaticCityAGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "StaticCityAGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STATICCITYA_API AStaticCityAGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
